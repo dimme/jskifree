43be947eecaa69c6aee9a9111c4fccb0e53edc6e
Dimitrios Vlastaras <mail@dimme.net>
2013-04-27 21:22:33 +0200

    Skier moves around

--- src/game/Skier.java
+++ src/game/Skier.java
@@ -6,17 +6,22 @@ import javax.swing.ImageIcon;
 
 public class Skier {
 
-    private String skier = "resources/sprites/skier1.png";
+    private Image up1 = new ImageIcon("resources/sprites/skier7.png").getImage();
+    private Image up2 = new ImageIcon("resources/sprites/skier8.png").getImage();
+    private Image down = new ImageIcon("resources/sprites/skier1.png").getImage();
+    private Image left = new ImageIcon("resources/sprites/skier6.png").getImage();
+    private Image right = new ImageIcon("resources/sprites/skier7.png").getImage();
+    
+    byte swifter = 0;
+    
+    private Image current = left;
 
     private int dx;
     private int dy;
     private int x;
     private int y;
-    private Image image;
 
     public Skier() {
-        ImageIcon ii = new ImageIcon(skier);
-        image = ii.getImage();
         x = 40;
         y = 60;
     }
@@ -36,7 +41,7 @@ public class Skier {
     }
 
     public Image getImage() {
-        return image;
+        return current;
     }
 
     public void keyPressed(KeyEvent e) {
@@ -45,18 +50,26 @@ public class Skier {
 
         if (key == KeyEvent.VK_LEFT) {
             dx = -1;
+            current = left;
         }
 
         if (key == KeyEvent.VK_RIGHT) {
             dx = 1;
+            current = right;
         }
 
         if (key == KeyEvent.VK_UP) {
             dy = -1;
+            swifter = (byte) ((swifter+1) % 2);
+            if (swifter == 0)
+            	current = up1;
+            else
+            	current = up2;
         }
 
         if (key == KeyEvent.VK_DOWN) {
             dy = 1;
+            current = down;
         }
     }
 
